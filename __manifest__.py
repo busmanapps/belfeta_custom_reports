2a95b22ff936c53d2492bf1c93e93188b6156976
Alderoto16 <144679671+Alderoto16@users.noreply.github.com>
2024-07-11 11:51:00 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': '<Nombre del módulo>',
+    'version': '<Versión>',
+    'description': '',
+    'summary': '',
+    'author': 'Busman View',
+    'website': 'https://busman.es',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'web'
+    ],
+    'data': [
+
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {}
+}
